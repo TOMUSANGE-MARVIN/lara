268daddf24563aab6d6e8c84c26b06f04049a114
marivin256 <marvintomusange@gmail.com>
2025-06-17 17:17:46 +0300

    4th commit

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+import 'bootstrap';
+import './bootstrap';
+
+// Your custom JavaScript code here
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './resources/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public/js'),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
